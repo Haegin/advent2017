8710bee7aa96a08dd816fae02bf24d6fa258bbc5
Harry Mills <harry@haeg.in>
2017-12-11 08:23:28 -0500

    More work on day 9

--- day9/part1.rb
+++ day9/part1.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+require "pry"
 require "treetop"
 
 Treetop.load "stream"
@@ -13,11 +14,19 @@ class Cleaner
   end
 
   def clean
-    @clean ||= @dirty.gsub(CANCELLED, '').gsub(GARBAGE, '')
+    @clean ||= @dirty.gsub(CANCELLED, '').gsub(GARBAGE, '').chomp
   end
 end
 
+def score(input)
+  clean = Cleaner.new(input).clean
+  puts clean
+  StreamParser.new.parse(clean).total_score
+end
+
+# raise unless score("{{<a!>},{<a!>},{<a!>},{<ab>}}") == 3
+# raise unless score("{{<ab>},{<ab>},{<ab>},{<ab>}}") == 9
+# raise unless score("{{{},{},{{}}}}") == 16
+
 input = File.read('./input')
-clean = Cleaner.new(input).clean
-parser = StreamParser.new
-puts parser.parse(clean).total_score
+puts score(input)
